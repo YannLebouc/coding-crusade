4b89f994a84a7d61533116040a3fcec641aaa022
Lebouc Yann <yann.leb251@gmail.com>
2025-04-13 09:29:52 +0200

    table creation exception handling

--- src/coding_crusade/db/create_table.py
+++ src/coding_crusade/db/create_table.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from coding_crusade.db.connection import get_connection
+from psycopg import DatabaseError
 
 sql_files_directory = os.path.dirname(os.path.realpath(__file__)) + "/sql/"
 # Ajouter un arguement obligatoire pour le script qui sera le nom de la table à créer
@@ -10,12 +11,18 @@ args = parser.parse_args()
 
 sql_file = sql_files_directory + args.table_name + ".sql"
 # Lire le fichier pour exécuter la requête avec la connection psycopg
-with open(sql_file) as f:
-    print(f)
+try:
+    with open(sql_file) as f:
+        sql_file_content = f.read()
+        try:
+            with get_connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(sql_file_content)
+                    print(cur.fetchone())
+        except DatabaseError as e:
+            print("An error occured during database operations, message : " + e)
+except Exception as e:
+    print("Something went wrong while trying to open the file, message : " + e)
 
-# with get_connection() as conn:
-#     with conn.cursor() as cur:
-# cur.execute("SELECT 1")
-# print(cur.fetchone())
 # try/except à chaque erreur posible
-# Retour de message de succès ou d'erre
+# Retour de message de succès ou d'erreur
