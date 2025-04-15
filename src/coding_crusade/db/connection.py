20f9aee59800af20586dbf0416397f51a9072324
Lebouc Yann <yann.leb251@gmail.com>
2025-04-15 07:11:34 +0200

    Added possibility of passing arguments to psycopg connection

--- src/coding_crusade/db/connection.py
+++ src/coding_crusade/db/connection.py
@@ -1,6 +1,7 @@
 import psycopg
+from typing import Any
 from coding_crusade.config import settings
 
 
-def get_connection():
-    return psycopg.connect(settings.DATABASE_URL)
+def get_connection(**kwargs: Any) -> psycopg.Connection:
+    return psycopg.connect(settings.DATABASE_URL, **kwargs)
