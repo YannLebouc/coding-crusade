98ce740829233f73e96ce52d0c849281311ed4d2
Lebouc Yann <yann.leb251@gmail.com>
2025-04-16 06:51:20 +0200

    Added logger, made function static and added return values after user insertion

--- src/coding_crusade/repositories/user.py
+++ src/coding_crusade/repositories/user.py
@@ -1,20 +1,35 @@
 from coding_crusade.db.connection import get_connection
 from coding_crusade.models.user import User
 from psycopg import DatabaseError, class_row
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class UserRepository:
 
+    @staticmethod
     def create_user(user: User) -> User:
         try:
             with get_connection(row_factory=class_row(User)) as conn:
                 result = conn.execute(
                     """
                         INSERT INTO users(username, password, email)
-                        VALUES (%s, %s, %s);
+                        VALUES (%s, %s, %s)
+                        RETURNING
+                            id
+                            ,username
+                            ,password
+                            ,email
+                            ,is_admin
+                            ,is_active
+                            ,created_at
+                            ,updated_at
+                        ;
                     """,
                     (user.username, user.password, user.email),
                 ).fetchone()
             return result
         except DatabaseError as e:
-            print("Erreur lors de la création de l'utilisateur " + e)
+            logger.error("Erreur lors de la création de l'utilisateur : %s", e)
+            raise
