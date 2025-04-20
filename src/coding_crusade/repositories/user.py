cb2a299579a3353bd0dbcec69c0a67236572d5cf
Lebouc Yann <yanlpi@proton.me>
2025-04-20 20:44:25 +0200

    Deleted exception handling from repository following my senpai's advices

--- src/coding_crusade/repositories/user.py
+++ src/coding_crusade/repositories/user.py
@@ -1,23 +1,38 @@
 from coding_crusade.db.connection import get_connection
 from coding_crusade.models.user import User
 from coding_crusade.dtos.user import UserResponse
-from psycopg import DatabaseError, class_row
-import logging
-
-logger = logging.getLogger(__name__)
+from psycopg import class_row
 
 
 class UserRepository:
 
     @staticmethod
     def create_user(user: User) -> UserResponse:
-        try:
+        with get_connection(row_factory=class_row(UserResponse)) as conn:
+            result = conn.execute(
+                """
+                    INSERT INTO users(username, password, email)
+                    VALUES (%s, %s, %s)
+                    RETURNING
+                        id
+                        ,username
+                        ,email
+                        ,is_admin
+                        ,is_active
+                        ,created_at
+                        ,updated_at
+                    ;
+                """,
+                (user.username, user.password, user.email),
+            ).fetchone()
+        return result
+
+        @staticmethod
+        def get_user_by_id(user: User) -> UserResponse | None:
             with get_connection(row_factory=class_row(UserResponse)) as conn:
                 result = conn.execute(
                     """
-                        INSERT INTO users(username, password, email)
-                        VALUES (%s, %s, %s)
-                        RETURNING
+                        SELECT 
                             id
                             ,username
                             ,email
@@ -25,18 +40,16 @@ class UserRepository:
                             ,is_active
                             ,created_at
                             ,updated_at
+                        FROM users
+                        WHERE id = %s
                         ;
                     """,
-                    (user.username, user.password, user.email),
+                    (user.id),
                 ).fetchone()
             return result
-        except DatabaseError as e:
-            logger.error("Erreur lors de la création de l'utilisateur : %s", e)
-            raise
 
-        @staticmethod
-        def get_user_by_id(user: User) -> UserResponse | None:
-            try:
+            @staticmethod
+            def get_user_by_email(user: User) -> UserResponse | None:
                 with get_connection(row_factory=class_row(UserResponse)) as conn:
                     result = conn.execute(
                         """
@@ -49,37 +62,9 @@ class UserRepository:
                                 ,created_at
                                 ,updated_at
                             FROM users
-                            WHERE id = %s
+                            WHERE email = %s
                             ;
                         """,
-                        (user.id),
+                        (user.email),
                     ).fetchone()
                 return result
-            except DatabaseError as e:
-                logger.error("Erreur lors de la récupération de l'utilisateur : %s", e)
-                raise
-
-            @staticmethod
-            def get_user_by_email(user: User) -> UserResponse | None:
-                try:
-                    with get_connection(row_factory=class_row(UserResponse)) as conn:
-                        result = conn.execute(
-                            """
-                                SELECT 
-                                    id
-                                    ,username
-                                    ,email
-                                    ,is_admin
-                                    ,is_active
-                                    ,created_at
-                                    ,updated_at
-                                FROM users
-                                WHERE email = %s
-                                ;
-                            """,
-                            (user.email),
-                        ).fetchone()
-                    return result
-                except DatabaseError as e:
-                    logger.error("Erreur lors de la création de l'utilisateur : %s", e)
-                    raise
